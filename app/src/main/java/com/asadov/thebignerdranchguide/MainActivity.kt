f0cf74c901972814a3e2129baf0663170a75e81c
Elmaddin <asadov.elmaddin@gmail.com>
2024-08-29 10:08:06 +0400

    commit

--- app/src/main/java/com/asadov/thebignerdranchguide/MainActivity.kt
+++ app/src/main/java/com/asadov/thebignerdranchguide/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.asadov.thebignerdranchguide
 
+import Question
 import android.os.Bundle
 
 import android.widget.Button
@@ -12,6 +13,16 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
